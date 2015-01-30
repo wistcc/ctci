e7271bde23e8fa1b7972670c80f9229f546d87cb
wistcc <wistcc@hotmail.com>
2015-01-30 11:41:02 -0400

    Initial commit

---
+++ c-sharp with Tests/Practice1/Practice 1.1.Tests/ProgramTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Practice_1._1.Tests
+{
+    [TestFixture]
+    public class ProgramTest
+    {
+        [Test]
+        public void StringIsUniqueShouldReturnTrue()
+        {
+            Assert.IsTrue(Program.StringIsUnique("abcdefg"));
+        }
+        [Test]
+        public void StringIsUniqueShouldReturnFalse()
+        {
+            Assert.IsFalse(Program.StringIsUnique("abcdcefgc"));
+        }
+    }
+}
---
+++ c-sharp with Tests/Practice1/Practice 1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Practice_1._1
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            if(StringIsUnique("abcdcefg"))
+            {
+                Console.WriteLine("It's unique");
+            }
+            else
+            {
+                Console.WriteLine("It's not unique");
+            }
+            Console.Read();
+        }
+
+        public static bool StringIsUnique(string stringToValidate)
+        {
+            for (var i = 0; i < stringToValidate.Length; i++)
+            {
+                for (var j = i+1; j < stringToValidate.Length; j++)
+                {
+                    if (stringToValidate[i] == stringToValidate[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
